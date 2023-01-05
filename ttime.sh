17052260bfec8f4d8e565f7be886e5bd1b99c525
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2023-01-06 03:19:53 +1100

    Update ttime.sh

--- ttime.sh
+++ ttime.sh
@@ -10,7 +10,7 @@ clear;
 sync;#time not found‽‽‽ :D
 dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)";#https://stackoverflow.com/a/29835459#https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html#tag_08_01
 file="$dir/ttime.log";
-date +1%F\ %T.%N >> "$file";
+date '+HE 1%F %T.%N%z %Z (%B, week %V, %A %j)' >> "$file";#sunrise,sunset,weather,.. (annual week, not anu week)
 tail "$file";
 echo;
 echo '/(key|mouse(clicks; to avoid value drift)|voice(recognition; to avoid value drift)|gesture(recognition; to avoid value drift)|eeg(recognition; to avoid value drift))logger/ makes this redundant; temporal shelllogs for autottime';
