ff28acf70e5048cb544f95398adb67f4c91af953
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2023-01-06 01:47:33 +1100

    Update ttime.sh

--- ttime.sh
+++ ttime.sh
@@ -3,7 +3,7 @@
 # "create ttime"
 # source.md files don't autowrap, source.language is better than built-in(look-a-like)s #varcase
 #todo:
-# security feature: full addressable paths; packages can crack env, and probably every"-thing" else anyway
+# security feature: full addressable paths; packages can crack env, and probably every"-thing" else anyway (`sudo rm -fr / # iff i can do it, they can do it through me`)
 clear;
 sync;#time not found‽‽‽ :D
 dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)";#https://stackoverflow.com/a/29835459#https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap08.html#tag_08_01
